280ef573abc0f45ff6ac9e8eb41205853b9e47d1
Sarisha Nimai Naidoo <sarishanimai@gmail.com>
2025-02-16 11:11:27 +0200

    Minor refactoring and updates

--- RIBAssessment.SarishaNaidoo/ClientApp/src/app/app.component.ts
+++ RIBAssessment.SarishaNaidoo/ClientApp/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'EmployeePortal';
+  title = 'Employee Portal';
 }
--- RIBAssessment.SarishaNaidoo/CustomExtenstionMiddleware/GlobalExceptionHandler.cs
+++ RIBAssessment.SarishaNaidoo/CustomExtenstionMiddleware/GlobalExceptionHandler.cs
@@ -13,13 +13,15 @@ namespace RIBAssessment.SarishaNaidoo.CustomExtenstionMiddleware
         }
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
+            _logger.LogError(exception, "An unhandled exception occurred: {Message}", exception.Message);
+
             httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
             httpContext.Response.ContentType = "application/json";
 
             await httpContext.Response.WriteAsync(new ErrorDetails
             {
                 StatusCode = httpContext.Response.StatusCode,
-                Message = "Internal Server Error 500"
+                Message = "An unexpected error occured. Please try again later."
             }.ToString());
 
             _logger.LogError("");
