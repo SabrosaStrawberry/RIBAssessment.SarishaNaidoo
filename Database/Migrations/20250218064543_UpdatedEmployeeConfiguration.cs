f3882dde2e5938528a4c00bcf639107cf0261b48
Sarisha Nimai Naidoo <sarishanimai@gmail.com>
2025-02-18 08:47:41 +0200

    Updated Seed Data

---
+++ Database/Migrations/20250218064543_UpdatedEmployeeConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEmployeeConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1,
+                column: "EmployeeNumber",
+                value: "ABC1234567891011");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2,
+                column: "EmployeeNumber",
+                value: "ASD1234567891011");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 3,
+                column: "EmployeeNumber",
+                value: "QWE1234567891011");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1,
+                column: "EmployeeNumber",
+                value: "ABC123");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2,
+                column: "EmployeeNumber",
+                value: "ASD345");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 3,
+                column: "EmployeeNumber",
+                value: "QWE456");
+        }
+    }
+}
